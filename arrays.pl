a1aad656e9d0132f894d4990059ae8336a8d34ad
Will Sheppard <1519594+willsheppard@users.noreply.github.com>
2024-06-30 17:05:21 +0100

    More checks and results

--- arrays.pl
+++ arrays.pl
@@ -38,7 +38,8 @@ sub state_array_inside {
     @foreachloop = @HH;
 }
 
-# Setup HH2
+# Workaround for the fact that you can't do list assignment initialisation of state variables
+# See https://stackoverflow.com/questions/6702666/why-cant-we-initialize-state-arrays-hashes-in-list-context
 state @HH2;
 state $HH2_is_initialized;
 if (! $HH2_is_initialized) {
@@ -46,12 +47,10 @@ if (! $HH2_is_initialized) {
     @HH2 = (qw/plants objects things other_thing more_things metadata/);
 }
 sub state_array_outside {
-    # Horrible workaround for the fact that you can't do list assignment initialisation of state variables
-    # See https://stackoverflow.com/questions/6702666/why-cant-we-initialize-state-arrays-hashes-in-list-context
-    #state @HH = (qw/plants objects things other_thing more_things metadata/);
     @foreachloop = @HH2;
 }
 
+# This is how EPSI does it (June 2024), it is NOT the fastest
 sub state_arrayref_inside {
     state $II = [qw/plants objects things other_thing more_things metadata/];
     @foreachloop = @$II;
@@ -62,29 +61,46 @@ sub state_arrayref_outside {
     @foreachloop = @$II2;
 }
 
+our @JJ = (qw/plants objects things other_thing more_things metadata/);
+sub our_array {
+    @foreachloop = @JJ;
+}
+
+our $JJ2 = [qw/plants objects things other_thing more_things metadata/];
+sub our_arrayref {
+    @foreachloop = @$JJ2;
+}
+
 # Sanity test for correct values. TODO: Automate this check
-#state_arrayref_outside();
+#our_arrayref();
 #print "LOOP = ".Dumper(\@foreachloop); use Data::Dumper;
 #__END__
 
 my $code = {
-    constant_native_array     => \&constant_native_array,
-    const_fast_array          => \&const_fast_array,
-    constant_readonly_array   => \&constant_readonly_array,
-    state_array_inside  => \&state_array_inside,
-    state_array_outside => \&state_array_outside,
-    state_arrayref_inside  => \&state_arrayref_inside,
-    state_arrayref_outside => \&state_arrayref_outside,
+    # Alphabetically sorted, as that's how the results come out
+    const_fast_array        => \&const_fast_array,          # Fast
+    constant_native_array   => \&constant_native_array,     # Very slow
+    constant_readonly_array => \&constant_readonly_array,   # Fast
+    our_array               => \&our_array,                 # Fast
+    our_arrayref            => \&our_arrayref,              # Slow
+    state_array_inside      => \&state_array_inside,        # Fast-ish
+    state_array_outside     => \&state_array_outside,       # Fast
+    state_arrayref_inside   => \&state_arrayref_inside,     # Slow
+    state_arrayref_outside  => \&state_arrayref_outside,    # Slow
 };
 
 timethese(20_000_000, $code);
 
-__DATA__
-const_fast_array:           15.3594 wallclock secs (15.35 usr +  0.00 sys = 15.35 CPU) @ 1302931.60/s (n=20000000)
-constant_native_array:      40.9162 wallclock secs (40.91 usr +  0.00 sys = 40.91 CPU) @ 488878.02/s (n=20000000)
-constant_readonly_array:    15.8882 wallclock secs (15.89 usr +  0.00 sys = 15.89 CPU) @ 1258653.24/s (n=20000000)
-state_array_inside:         16.2704 wallclock secs (16.27 usr +  0.00 sys = 16.27 CPU) @ 1229256.30/s (n=20000000)
-state_array_outside:        15.2003 wallclock secs (15.20 usr +  0.00 sys = 15.20 CPU) @ 1315789.47/s (n=20000000)
-state_arrayref_inside:      26.1554 wallclock secs (26.15 usr +  0.00 sys = 26.15 CPU) @ 764818.36/s (n=20000000)
-state_arrayref_outside:     25.4062 wallclock secs (25.39 usr +  0.02 sys = 25.41 CPU) @ 787091.70/s (n=20000000)
+__END__
+
+Results:
+          const_fast_array: 15.5332 wallclock secs (15.53 usr +  0.00 sys = 15.53 CPU) @ 1287830.01/s (n=20000000)
+     constant_native_array: 40.5338 wallclock secs (40.52 usr +  0.01 sys = 40.53 CPU) @ 493461.63/s (n=20000000)
+   constant_readonly_array: 15.339  wallclock secs (15.33 usr +  0.00 sys = 15.33 CPU) @ 1304631.44/s (n=20000000)
+                 our_array: 15.5363 wallclock secs (15.53 usr +  0.00 sys = 15.53 CPU) @ 1287830.01/s (n=20000000)
+              our_arrayref: 25.3385 wallclock secs (25.33 usr +  0.00 sys = 25.33 CPU) @ 789577.58/s (n=20000000)
+        state_array_inside: 16.0206 wallclock secs (16.02 usr +  0.00 sys = 16.02 CPU) @ 1248439.45/s (n=20000000)
+       state_array_outside: 15.3056 wallclock secs (15.30 usr +  0.00 sys = 15.30 CPU) @ 1307189.54/s (n=20000000)
+     state_arrayref_inside: 26.0376 wallclock secs (26.04 usr +  0.00 sys = 26.04 CPU) @ 768049.16/s (n=20000000)
+    state_arrayref_outside: 25.4501 wallclock secs (25.42 usr +  0.03 sys = 25.45 CPU) @ 785854.62/s (n=20000000)
 
